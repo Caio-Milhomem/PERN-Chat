9f1902dee9c8b14891fd0aa93c4343ee81941eaa
Caio Milhomem <cmilhomem@network.local>
2024-09-19 11:36:09 -0300

    Messages routes and controllers added

--- backend/src/routes/messages.route.ts
+++ backend/src/routes/messages.route.ts
@@ -1,6 +1,16 @@
 import express from "express";
+import protectRoute from "../middleware/protectRoute.js";
+import {
+  sendMessage,
+  getMessages,
+  getUsersForSidebar,
+} from "../controllers/message.controller.js";
 
 const router = express.Router();
+
+router.get("/conversations", protectRoute, getUsersForSidebar);
+router.get("/:id", protectRoute, getMessages);
+router.post("/send/:id", protectRoute, sendMessage);
 
 router.get("/conversations", (req, res) => {
   res.send("Conversation route");
